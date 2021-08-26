031dc1468da252d25560e4c2ba14a40d07f146c6
LeeJack123 <taol3318@163.com>
2021-08-26 10:53:11 +0800

    三次提交

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -4,6 +4,9 @@ public class TestGit {
 	public static void main(String[] args) {
 		
 		System.out.println("二次提交");
+		System.out.println("二次提交");
+		System.out.println("二次提交");
+		System.out.println("二次提交");
 	}
 
 }
---
+++ src/main/java/com/atguigu/git/TestGit01.java
@@ -0,0 +1,10 @@
+package com.atguigu.git;
+
+public class TestGit01 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("三次提交");
+	}
+
+}
