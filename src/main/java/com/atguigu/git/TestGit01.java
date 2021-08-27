15f6bf95dab68609180041e2a9207e1934d696ee
LeeJack123 <taol3318@163.com>
2021-08-27 10:09:39 +0800

    merge

--- src/main/java/com/atguigu/git/TestGit01.java
+++ src/main/java/com/atguigu/git/TestGit01.java
@@ -5,6 +5,12 @@ public class TestGit01 {
 	public static void main(String[] args) {
 		
 		System.out.println("三次提交");
+		System.out.println("三次提交");
+		System.out.println("三次提交");
+		System.out.println("三次提交");
+		System.out.println("hot_fixhot_fix");
+		System.out.println("hot_fix");
+		System.out.println("hot_fix");
 	}
 
 }
